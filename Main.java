96061ad15a86bae4b43bd7e33e8b83233589d953
26dmc <165817947+26dmc@users.noreply.github.com>
2024-04-05 08:53:23 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.until.*;
+class Starts{
+   public static void main (string[]args){
+
+     Scanner input=new Scanner(System.in);
+     
+     System.out.println("How many stars would you like to see?");
+     int starts= input.nextint();
+
+
+     while(num<stars){
+       System.out.ptintln("*");
+     }
+     System.out.println("Enjoy your evening!");
+   }
+}
